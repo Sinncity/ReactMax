59f536c0fdfeba160bfb590ff35c5867b20d9b29
igpar <igpar@Tparker85.localdomain>
2022-05-18 10:21:51 -0600

    first check point of project

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import ExpenseItem from "../Expenses/ExpenseItem";
+import Card from "../UI/Card";
+import "./Expense.css";
+
+const Expense = (props) => {
+  return (
+    <Card className="expense">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
+};
+
+export default Expense;
