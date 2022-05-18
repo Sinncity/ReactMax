c21cb81dc1eca0c620678b0b1cde4681e047a8b2
igpar <igpar@Tparker85.localdomain>
2022-05-18 04:48:55 -0600

    first react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code></code>
+          <ExpenseItem First custom Component />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
